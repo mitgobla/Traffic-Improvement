52e49e2b568648f7d2bcf6c9a76e5c49acfb90b2
Edward Upton <ed.upton@outlook.com>
2019-03-16 15:43:48 +0000

    Completed web interface

---
+++ Traffic-Light-Management/main.py
@@ -0,0 +1,155 @@
+import threading
+import numpy as np
+import os
+import io
+
+from flask import Flask, request, render_template, redirect, Response
+from flask_classful import FlaskView, route
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.mlab import griddata
+from matplotlib.offsetbox import AnchoredText
+from mpl_toolkits.mplot3d import Axes3D
+import scipy.interpolate as interp
+
+import traffic_simulator
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+debug = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+class HomeView(FlaskView):
+    route_base='/home'
+
+    def index(self):
+        return render_template("index.html")
+    
+
+class GetTimingView(FlaskView):
+    route_base='/get-timings'
+
+    def index(self):
+        return render_template("env_setup.html")
+
+class SimulateView(FlaskView):
+    route_base='/simulate'
+
+    @route('/submit', methods=['GET', 'POST'])
+    def submit_simulation(self):
+        self.distanceLtoL = float(request.form["distanceLtoL"])
+        self.speedLimitLtoL = float(request.form["speedLimitLtoL"])
+        self.timeUpQueue = float(request.form["timeUpQueue"])
+        if "lightsDetectMovement" in request.form:
+            self.lightsDetectMovement = True
+        else:
+            self.lightsDetectMovement = False
+        self.simTime = int(request.form["simTime"])
+        self.lightGreenTimesRange = [int(request.form["lightGreenTimeStart"]), int(request.form["lightGreenTimeEnd"])]
+        self.roadUsagesRange = [float(request.form["roadUsageStart"]), float(request.form["roadUsageEnd"])]
+        self.simulationThread = SimulationThread(self.distanceLtoL, self.speedLimitLtoL, self.timeUpQueue, self.lightsDetectMovement, self.simTime, self.lightGreenTimesRange, self.roadUsagesRange)
+        self.simulationThread.name = "simThread"
+        self.simulationThread.start()
+        return redirect("/simulate/simulation-running")
+    
+    @route('/simulation-running')
+    def running_simulation(self):
+        response = Response(redirect('/simulate/simulation-results'))
+        activeThreadNames = list(thread.name for thread in threading.enumerate())
+        if "simThread" in activeThreadNames:
+            simThread = threading.enumerate()[activeThreadNames.index("simThread")]
+            return render_template('simulation_running.html', currentIteration=simThread.currentIteration, totalIterations=simThread.numberOfIterations, percentageDone=round((simThread.currentIteration/simThread.numberOfIterations) * 100), distanceLtoL=simThread.distanceLtoL, speedLimitLtoL=simThread.speedLimitLtoL, timeUpQueue=simThread.timeUpQueue, simTime=simThread.simTime, lightsDetectMovement=simThread.lightsDetectMovement)
+        else:
+            return redirect('/simulate/simulation-results')
+
+    @route('/simulation-results')
+    def finished_simulation(self):
+        return render_template('simulation_results.html')
+
+
+class SimulationThread(threading.Thread):
+    def __init__(self, distanceLtoL, speedLimitLtoL, timeUpQueue, lightsDetectMovement, simTime, lightGreenTimesRange, roadUsagesRange):
+        threading.Thread.__init__(self)
+        self.distanceLtoL = distanceLtoL
+        self.speedLimitLtoL = speedLimitLtoL
+        self.timeUpQueue = timeUpQueue
+        self.lightsDetectMovement = lightsDetectMovement
+        self.simTime = simTime
+        self.lightGreenTimesRange = lightGreenTimesRange
+        self.roadUsageRange = roadUsagesRange
+        self.timeGreenStep = 2
+        self.roadUsageStep = 0.02
+
+        self.numberOfIterations = round((((self.roadUsageRange[1]-self.roadUsageRange[0])/self.roadUsageStep)+2)*(((self.lightGreenTimesRange[1]-self.lightGreenTimesRange[0])/self.timeGreenStep)+2))
+    
+    def run(self):
+        self.currentIteration = 0
+        x = []
+        y = []
+        z = []
+        for timeGreen in range(self.lightGreenTimesRange[0], self.lightGreenTimesRange[1]+self.timeGreenStep, self.timeGreenStep):
+            for roadUsage in np.arange(self.roadUsageRange[0], self.roadUsageRange[1]+self.roadUsageStep, self.roadUsageStep):
+                self.currentIteration += 1
+                tenv = traffic_simulator.TrafficEnvironment()
+                tenv.set_env_variables(self.distanceLtoL, self.speedLimitLtoL, self.timeUpQueue, self.lightsDetectMovement, self.simTime, timeGreen, roadUsage)
+
+                tenv.start_simulation()
+                if tenv.averageTimeStopped <= 60:
+                    z.append(tenv.averageTimeStopped)
+                else:
+                    z.append(60)
+                x.append(roadUsage)
+                y.append(timeGreen)
+
+        minimasx = []
+        minimasy = []
+        minimasz = []
+        for roadUsage in np.arange(self.roadUsageRange[0], self.roadUsageRange[1]+0.02, 0.02):
+            indexesAtCurrentRoadUsage = []
+            for index1 in range(len(x)):
+                if x[index1] == roadUsage:
+                    indexesAtCurrentRoadUsage.append(index1)
+            tempZList = []
+            tempYList = []
+            for index2 in indexesAtCurrentRoadUsage:
+                tempZList.append(z[index2])
+                tempYList.append(y[index2])
+            minIndex = np.argmin(tempZList)
+            minimasx.append(roadUsage)
+            minimasy.append(tempYList[minIndex])
+            minimasz.append(tempZList[minIndex])
+
+        plotx,ploty = np.meshgrid(np.linspace(np.min(x),np.max(x),50),\
+                                np.linspace(np.min(y),np.max(y),50))
+        plotz = interp.griddata((x,y),z,(plotx,ploty), method='linear')
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(minimasx, minimasy, minimasz, '--k', label="Most Efficient")
+        ax.legend()
+        surf = ax.plot_surface(plotx,ploty,plotz,cstride=1,rstride=1,cmap=cm.jet)
+
+
+        cbar = fig.colorbar(surf, shrink=0.5, aspect=10)
+        ax.set_title("lightGreenTime vs. roadUsage")
+        ax.set_xlabel('roadUsage') 
+        ax.set_ylabel('lightGreenTime')
+        ax.set_zlabel('averageTimeStopped')
+        ax.view_init(elev=30, azim=-150)
+        buf = io.BytesIO()
+        with open(os.path.join(CWD, "static/images/tempGraph.svg"), "wb") as tempGraphFile:
+            plt.savefig(tempGraphFile, format='svg')
+        plt.close()
+        plt_bytes = buf.getvalue()
+        buf.close()
+        self.graphImageBytes = plt_bytes
+
+
+HomeView.register(app)
+GetTimingView.register(app)
+SimulateView.register(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=debug)
---
+++ Traffic-Light-Management/traffic_simulator.py
@@ -0,0 +1,179 @@
+import itertools
+import json
+import math
+import os
+import random
+import time
+
+import numpy as np
+import simpy
+
+class TrafficEnvironment(object):
+    def __init__(self):
+        self.environment = simpy.Environment()
+        self.trafficLightsToCreate = [["A"],["B"]]
+
+        self.vehiclesList = []
+        self.trafficLightsList = []
+    
+    def set_env_variables(self, distanceLtoL, speedLimitLtoL, timeUpQueue, trafficLightDetectMovement, simTime, timeGreen, roadUsage):
+        """Set Environment Variables
+        
+        Keyword Arguments:
+        distanceLtoL -- The distance between the two lights
+        speedLimitLtoL -- The speed limit (mph) between the two lights
+        timeUpQueue -- Time it takes a vehicle to move up the queue
+        trafficLightDetectMovement -- Can the traffic light detect if no vehicles are moving
+        simTime -- How long to simulate this iteration form
+        timeGreen -- How long is each traffic light green
+        roadusage -- How many vehicles are added per second
+        """
+        
+        self.timeLtoL = distanceLtoL / (speedLimitLtoL * 0.44704)
+        self.timeUpQueue = timeUpQueue
+        self.trafficLightDetectMovement = trafficLightDetectMovement
+        self.simTime = simTime
+        self.timeGreen = timeGreen
+        self.roadUsage = roadUsage
+
+
+
+    def start_simulation(self):
+        self.create_system()
+
+        self.environment.run(until=self.simTime)
+
+        # print(len(self.trafficLightsList[0].vehiclesAtLight))
+        # print(len(self.trafficLightsList[1].vehiclesAtLight))
+
+        totalTimeStopped = 0
+        vehicleCount = 0
+        for vehicle in self.vehiclesList:
+            if hasattr(vehicle, "timeStopped"):
+                totalTimeStopped += vehicle.timeStopped
+                vehicleCount += 1
+        self.averageTimeStopped = totalTimeStopped / vehicleCount
+        # print("Average Time Stopped:", (totalTimeStopped/vehicleCount))
+
+    def create_system(self):
+        for trafficLight in self.trafficLightsToCreate:
+            
+            self.trafficLightsList.append(TrafficLight(self, trafficLight[0]))
+
+        self.roadBetweenLights = RoadBetweenLights(self)
+
+        self.tmgmt = TrafficManagement(self)
+        self.environment.process(self.generate_vehicles())
+
+    def generate_vehicles(self):
+        i = len(self.vehiclesList)
+        while True:
+            if random.random() < self.roadUsage:
+                self.vehiclesList.append(Vehicle(self, str(i), self.trafficLightsList[random.randint(0, len(self.trafficLightsList)-1)]))
+                i += 1
+            yield self.environment.timeout(1)
+
+class TrafficManagement:
+    def __init__(self, tenv):
+        self.tenv = tenv
+        self.tenv.environment.process(self.cycle_light_states())
+    
+    def cycle_light_states(self):
+        while True:
+            for light in self.tenv.trafficLightsList:
+                yield self.tenv.roadBetweenLights.isEmpty
+                yield self.tenv.environment.timeout(1)
+                light.change_light_state()
+                light.checkForMovement = True
+                self.tenv.environment.process(light.no_movement_detection())
+                yield self.tenv.environment.any_of([self.tenv.environment.timeout(self.tenv.timeGreen)])
+                light.checkForMovement = False
+                light.change_light_state()
+
+    
+
+
+
+class TrafficLight:
+    def __init__(self, tenv, identity):
+        self.tenv = tenv
+        self.identity = identity
+        self.vehiclesAtLight = []
+        self.states = itertools.cycle(['red', 'green'])
+        self.currentState = next(self.states)
+        self.lightGreenEvent = self.tenv.environment.event()
+        self.lightRedEvent = self.tenv.environment.event()
+        self.noMovementEvent = self.tenv.environment.event()
+        self.checkForMovement = False
+
+    def change_light_state(self):
+        self.currentState = next(self.states)
+        if self.currentState == 'green':
+            self.lightGreenEvent.succeed()
+            self.lightRedEvent = self.tenv.environment.event()
+        if self.currentState == 'red':
+            self.lightRedEvent.succeed()
+            self.lightGreenEvent = self.tenv.environment.event()
+
+    def no_movement_detection(self):
+        if self.tenv.trafficLightDetectMovement:
+            while self.checkForMovement == True:
+                if len(self.vehiclesAtLight) == 0:
+                    yield self.tenv.environment.timeout(1)
+                    if len(self.vehiclesAtLight) == 0:
+                        self.noMovementEvent.succeed() 
+                yield self.tenv.environment.timeout(1)
+                self.noMovementEvent = self.tenv.environment.event()
+
+class Vehicle:
+    def __init__(self, tenv, identity, location):
+        self.tenv = tenv
+        self.identity = identity
+        self.location = location
+        self.location.vehiclesAtLight.append(self)
+        self.tenv.environment.process(self.run())
+        self.timeWhenStopped = self.tenv.environment.now
+
+    def run(self):
+        if self.location.vehiclesAtLight.index(self) == 0:
+            self.moved = self.tenv.environment.event()
+            yield self.location.lightGreenEvent            
+            yield self.tenv.environment.process(self.travel_between_lights())            
+        else: 
+            self.moved = self.tenv.environment.event()
+            yield self.location.vehiclesAtLight[self.location.vehiclesAtLight.index(self) - 1].moved
+            yield self.tenv.environment.process(self.travel_up_queue())
+
+    def travel_between_lights(self):
+        # print("Car", self.identity, "is moving between lights from", self.location.identity)
+        self.tenv.roadBetweenLights.add_vehicle(self)
+        self.moved.succeed()
+        self.location.vehiclesAtLight.pop(self.location.vehiclesAtLight.index(self))
+        self.timeStopped = self.tenv.environment.now - self.timeWhenStopped
+        # print(self.timeStopped)
+        yield self.tenv.environment.timeout(self.tenv.timeLtoL)
+        self.tenv.roadBetweenLights.remove_vehicle(self)
+    
+    def travel_up_queue(self):
+        # # print("Car", self.identity, "is moving up queue", self.location.identity)
+        yield self.tenv.environment.timeout(0.5)
+        yield self.tenv.environment.timeout(self.tenv.timeUpQueue)
+        self.moved.succeed()
+        self.tenv.environment.process(self.run())
+
+class RoadBetweenLights:
+    def __init__(self, tenv):
+        self.tenv = tenv
+        self.vehiclesBetweenLights = []
+        self.isEmpty = self.tenv.environment.event()
+        self.isEmpty.succeed()
+    
+    def add_vehicle(self, vehicle):
+        self.isEmpty = self.tenv.environment.event()
+        self.vehiclesBetweenLights.append(vehicle)
+
+    def remove_vehicle(self, vehicle):
+        self.vehiclesBetweenLights.pop(self.tenv.roadBetweenLights.vehiclesBetweenLights.index(vehicle))
+        if len(self.vehiclesBetweenLights) == 0:
+            # print("Allowing Lights to change")
+            self.isEmpty.succeed()
