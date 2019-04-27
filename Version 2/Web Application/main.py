c37ea0a9074227dc2b7179044477b8585fecfe9f
engiego <ed.upton@outlook.com>
2019-04-27 22:29:56 +0100

    Created Web Interface and Optimiser

--- Version 2/Web Application/main.py
+++ Version 2/Web Application/main.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, request, render_template, redirect, Response
 from flask_classful import FlaskView, route
+import threading
+
+import traffic_env_optimising
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 
@@ -8,11 +11,86 @@ debug = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+LIGHT_SENSOR_SENSITIVITY = 2
+
 class HomeView(FlaskView):
-    route_base='/'
+    route_base='/home'
 
     def index(self):
-        return render_template('home.html')
+        return render_template("home.html")
+
+class GetTimingsView(FlaskView):
+    route_base='/get-timings'
+
+    def index(self):
+        return render_template("get-timings.html")
+
+    @route('/submit', methods=['GET', 'POST'])
+    def submit(self):
+        self.lightDistance = float(request.form["distanceBetweenLightsRange"])
+        self.speed = float(request.form["speedRange"])
+        self.timeUpQueue = float(request.form["queueTimeRange"])
+        self.light1Busyness = float(request.form["busyness1Range"])
+        self.light2Busyness = float(request.form["busyness2Range"])
+        if "sensor1Check" in request.form:
+            self.light1SensorSensitivity = LIGHT_SENSOR_SENSITIVITY
+        else:
+            self.light1SensorSensitivity = -1
+
+        if "sensor2Check" in request.form:
+            self.light2SensorSensitivity = LIGHT_SENSOR_SENSITIVITY
+        else:
+            self.light2SensorSensitivity = -1
+
+        self.envTime = float(request.form["envTime"])
+        self.greenTimeRange = [float(request.form.getlist('greenTimeRange')[0]), float(request.form.getlist('greenTimeRange')[1])]
+        self.greenTimeStep = float(request.form["greenTimeStep"])
+
+        self.environmentData = {'lightDistance':self.lightDistance,
+                                'speed':self.speed,
+                                'timeUpQueue':self.timeUpQueue,
+                                'lightData':[{'busyness':self.light1Busyness,
+                                                'sensorSensitivity':self.light1SensorSensitivity},
+                                                {'busyness':self.light2Busyness,
+                                                'sensorSensitivity':self.light2SensorSensitivity}]}
+        self.optimisationData = {'envTime':self.envTime,
+                                'lightGreenTimeRange':self.greenTimeRange,
+                                'lightGreenTimeStep':self.greenTimeStep,
+                                'iterationsPerSetting':5}
+
+        self.simulationThread = SimulationThread(self.environmentData, self.optimisationData)
+        self.simulationThread.name = "simThread"
+        self.simulationThread.start()
+        return redirect("/get-timings/optimisation-running")
+
+    @route('/optimisation-running')
+    def running_optimisation(self):
+        activeThreadNames = list(thread.name for thread in threading.enumerate())
+        if "simThread" in activeThreadNames:
+            simThread = threading.enumerate()[activeThreadNames.index("simThread")]
+            return render_template('optimisation-running.html', envData = simThread.envData, optData = simThread.optData)
+        else:
+            return redirect('/get-timings/results')
+    
+    @route('/results')
+    def finished_optimising(self):
+        with open(os.path.join(CWD, 'TempData', 'tempOptimisationData.txt'), 'r') as tempData:
+            dataString = tempData.read()
+        bestLightGreenTime = float(dataString.split(',')[0])
+        lowestWaitingTime = float(dataString.split(',')[1])
+        graphFileName = dataString.split(',')[2]
+        return render_template('optimisation-results.html', bestTiming = str(bestLightGreenTime), leastWaitingTime = str(round(lowestWaitingTime, 1)), graphName = graphFileName)
+
+class SimulationThread(threading.Thread):
+    def __init__(self, envData, optData):
+        threading.Thread.__init__(self)
+        self.envData = envData
+        self.optData = optData
+    def run(self):
+        self.optimisationResults = traffic_env_optimising.run_optimisation(self.envData, self.optData)
+
+HomeView.register(app)
+GetTimingsView.register(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=debug)
---
+++ Version 2/Web Application/traffic_env_optimising.py
@@ -0,0 +1,168 @@
+import salabim as sim
+import time
+import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline, BSpline
+import random
+import numpy as np
+import uuid
+import os
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+
+VIEWPORT_RESOLUTION = [2560,1600]
+PERCENTAGE_TIME_SAFETY_ADDITION = 0.2
+STATE_COLOURS_DICT = {"states":[{"state":"red", "rgb":(255,0,0)},{"state":"redamber","rgb":(255,191,0)},{"state":"green", "rgb":(0,255,0)},{"state":"amber","rgb":(255,191,0)}]}
+
+class VehicleSpawner(sim.Component):
+    def setup(self, trafficEnv):
+        self.trafficEnv = trafficEnv
+        self.lightList = trafficEnv.lightList
+        self.randomUniform = sim.Uniform(0,1)
+    def process(self):
+        while True:
+            for light in self.lightList:
+                if light.busyness > self.randomUniform.sample():
+                    Vehicle(light=light, trafficEnv=self.trafficEnv)
+            yield self.hold(1)
+
+class TrafficManagement(sim.Component):
+    def setup(self, trafficEnv):
+        self.trafficEnv = trafficEnv
+        self.lightList = trafficEnv.lightList
+        self.timeLightGreen = trafficEnv.timeLightGreen
+        self.timeLtoLSafety = trafficEnv.timeLtoLSafety
+
+    def process(self):
+        while True:
+            for light in self.lightList:
+                yield self.hold(self.timeLtoLSafety)
+                light.change_state(state='amber')
+                yield self.hold(1)
+                light.change_state(state='green')
+                yield self.wait((light.movementState, 'none'), fail_delay=self.timeLightGreen)
+                light.change_state(state='amber')
+                yield self.hold(1)
+                light.change_state(state='red')
+
+
+class TrafficEnvironment():
+    def __init__(self, envData, timeLightGreen):
+        self.lightList = []
+        for lightNum in range(2):
+            self.lightList.append(Light(sensitivity=envData['lightData'][lightNum]['sensorSensitivity'], busyness=envData['lightData'][lightNum]['busyness']))
+        self.roadBetween = RoadBetween()
+        self.distanceLtoL = envData['lightDistance']
+        self.speed = envData['speed'] / 2.237
+        self.timeLtoL = self.distanceLtoL / self.speed
+        self.timeLtoLSafety = self.timeLtoL * (1 + PERCENTAGE_TIME_SAFETY_ADDITION)
+        self.timeLightGreen = timeLightGreen
+        self.timeUpQueue = envData['timeUpQueue']
+
+        self.vehicleSpawner = VehicleSpawner(trafficEnv=self)
+        self.trafficManagement = TrafficManagement(trafficEnv=self)
+
+
+class RoadBetween(sim.Component):
+    def setup(self):
+        self.vehiclesInBetweenBool = sim.State(
+            self.name() + ".vehiclesPresentState", value=False)
+        self.vehiclesQueue = sim.Queue(
+            self.name() + ".vehiclesQueue")
+
+
+class Light(sim.Component):
+    def setup(self, sensitivity, busyness):
+        self.state = sim.State((self.name() + ".state"), value="red")
+        self.vehiclesQueue = sim.Queue(self.name() + ".queue")
+        self.movementSensorSensitivity = sensitivity
+        self.movementState = sim.State(self.name() + ".movementState", value='movement')
+        self.busyness = busyness
+
+    def process(self):
+        while True:
+            if self.movementSensorSensitivity == -1:
+                pass
+            else:
+                if len(self.vehiclesQueue) == 0:
+                    yield self.hold(self.movementSensorSensitivity)
+                    if len(self.vehiclesQueue) == 0:
+                        self.movementState.set('none')
+                elif len(self.vehiclesQueue) > 0:
+                    self.movementState.set('movement')
+            yield self.hold(0.1)
+
+    def change_state(self, state):
+        self.state.set(state)
+
+
+class Vehicle(sim.Component):
+    def setup(self, light, trafficEnv):
+        self.trafficEnv = trafficEnv
+        self.roadBetween = trafficEnv.roadBetween
+        self.atLight = light
+        self.movedState = sim.State(self.name() + ".movedState")
+        self.atLight.vehiclesQueue.add(self)
+
+    def process(self):
+        while True:
+            if self.atLight.vehiclesQueue.index(self) == 0:
+                yield self.wait((self.atLight.state, 'green'))
+                # Travelling Between Lights
+                self.roadBetween.vehiclesQueue.add(self)
+                self.roadBetween.vehiclesInBetweenBool.set(True)
+                self.atLight.vehiclesQueue.remove(self)
+                self.movedState.set()
+                yield self.hold(self.trafficEnv.timeLtoL)
+                self.roadBetween.vehiclesQueue.remove(self)
+                if len(self.roadBetween.vehiclesQueue) == 0:
+                    self.roadBetween.vehiclesInBetweenBool.set(False)
+            elif self not in self.atLight.vehiclesQueue:
+                break
+            else:
+                vehicleInFront = self.atLight.vehiclesQueue[self.atLight.vehiclesQueue.index(self)-1]
+                yield self.wait(vehicleInFront.movedState)
+                # Moving Up Queue
+                yield self.hold(self.trafficEnv.timeUpQueue)
+                self.movedState.set()
+                yield self.hold(0.01)
+                self.movedState.set(False)
+                self.process()
+
+def run_optimisation(envData, optData):
+    dataArray = []
+    for lightGreenTime in range(int(optData['lightGreenTimeRange'][0]), int(optData['lightGreenTimeRange'][1]), int(optData['lightGreenTimeStep'])):
+        runningAverageTotal = 0
+        for iter in range(int(optData['iterationsPerSetting'])):
+            averageWaitingTime = 0
+            sim.random_seed = time.time()
+            print("Current Light Green Time:,", lightGreenTime, ", Iteration:", iter)
+            env = sim.Environment(trace=False, random_seed=time.time())
+            trafficEnv = TrafficEnvironment(envData, lightGreenTime)
+            env.run(int(optData['envTime']))
+            averageWaitingTime = sum([trafficEnv.lightList[0].vehiclesQueue.length_of_stay.mean(), trafficEnv.lightList[1].vehiclesQueue.length_of_stay.mean()]) / 2
+            runningAverageTotal += averageWaitingTime
+            print("Average Waiting Time For Car:", averageWaitingTime)
+        dataArray.append([lightGreenTime, runningAverageTotal/int(optData['iterationsPerSetting'])])
+
+    x, y = zip(*dataArray)
+
+    ymin = min(y)
+    xmin = x[y.index(ymin)]
+
+    print(ymin)
+    print(xmin)
+    # xnew = np.linspace(np.array(x).min(), np.array(x).max(), 300)
+
+    # spl = make_interp_spline(np.array(x), np.array(y), k=3)
+    # ynew = spl(xnew)
+    print(x, y)
+
+    plt.plot(x, y, color='g')
+    plt.xlabel('Light Green Time')
+    plt.ylabel('Average Waiting Time')
+    pltFileName = str(uuid.uuid4().hex) + '.png'
+    with open(os.path.join(CWD, 'static', 'images', pltFileName), 'wb') as tempPltImgFile:
+        plt.savefig(tempPltImgFile)
+    with open(os.path.join(CWD, 'TempData', 'tempOptimisationData.txt'), 'w') as tempDataFile:
+        tempDataFile.write(str(xmin) + ',' + str(ymin) + ',' + pltFileName)
+    return [xmin, ymin, pltFileName]
